f35bad9fb346c90284576b96fe90225a88fb8acd
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-01-29 22:49:13 -0300

    feat: StartClass files management

--- src/pages/StartClass/StartClass.tsx
+++ src/pages/StartClass/StartClass.tsx
@@ -1,26 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import { IoClose } from 'react-icons/io5';
+import { FaRegTrashAlt, FaRegEdit  } from "react-icons/fa";
 import {
   Container,
+  Title,
   Button,
   GeneratedCode,
   ContentPrimary,
+  FilePanel,
+  PanelHeaderContainer,
+  FilesList,
+  File,
+  Link,
+  ButtonsContainer,
   ContentSecondary,
+  FormContainer,
+  InputContainer,
+  SelectInput,
+  Error,
+  SubmitButton
 } from './styles';
 
+const validations = Yup.object({
+  team: Yup.string()
+    .required('Selecione uma turma')
+})
+
 const StartClass: React.FC = () => {
+  const [isPanelVisible, setIsPanelVisible] = useState(false)
+  const [code, setCode] = useState('')
+  const currentDate = new Date()
+  const day = currentDate.getDate()
+  const month = currentDate.getMonth() + 1
+  const year = currentDate.getFullYear()
+  const formattedDate = `${day}/${String(month).padStart(2, '0')}/${year}`
+
+  const MockTeams = [
+    { name: "Turma A - 1º Ano", room: "Sala 101", qntStudents: 25, shift: "Manhã", status: "ativa" },
+    { name: "Turma B - 1º Ano", room: "Sala 202", qntStudents: 20, shift: "Tarde", status: "ativa" },
+    { name: "Turma C - 2º Ano", room: "Sala 303", qntStudents: 18, shift: "Noite", status: "inativa" },
+    { name: "Turma D - 2º Ano", room: "Sala 404", qntStudents: 30, shift: "Manhã", status: "ativa" },
+    { name: "Turma A - 3º Ano", room: "Sala 505", qntStudents: 22, shift: "Tarde", status: "inativa" },
+  ];
+
+  const MockFiles = [
+    { name: "Segunda guerra mundial", type:"YouTube", url: "https://www.youtube.com/watch?v=Vq9oIj2ecU8&pp=ygUWc2VndW5kYSBndWVycmEgbXVuZGlhbA%3D%3D" },
+    { name: "Resumo Segunda guerra mundial", type:"PDF", url: "https://irp-cdn.multiscreensite.com/b422576b/files/uploaded/Resumo%20da%20Segunda%20Guerra%20Mundial.pdf" }
+  ]
+
+  const generateCode = () => {
+    const min = 100000
+    const max = 999999
+    setCode(String(Math.floor(Math.random() * (max - min + 1)) + min))
+  }
+  
+  const handleStart = (values) => {
+    console.log(values)
+  }
+
+  const showFiles = () => {
+    setIsPanelVisible(true)
+  };
+    
+  const hideFiles = () => {
+    setIsPanelVisible(false)
+  };
+
   return (
     <Container>
+      <Title>Aula do dia { formattedDate }</Title>
       <ContentPrimary>
-        <Button>Selecionar Turma</Button>
+        <FormContainer>
+          <Formik
+            initialValues={{ team: "" }}
+            enableReinitialize={true}
+					  validationSchema={validations}
+            onSubmit={(values) => handleStart(values)}
+          >
+            {({ values, handleChange, touched, errors }) => (
+              <Form style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                <InputContainer>
+                  <SelectInput
+                    as="select"
+                    name="team"
+                    id="team"
+                    onChange={handleChange}
+                    value={values.team}
+                  >
+                    <option value="">Selecione uma Turma</option>
+                    {MockTeams.map((team, index) => (
+                      <option key={index} value={team.name}>
+                        {team.name} - {team.room} ({team.shift})
+                      </option>
+                    ))}
+                  </SelectInput>
+                  {touched.team && errors.team && <Error>{errors.team}</Error>}
+                </InputContainer>
+                <SubmitButton type="submit">Iniciar</SubmitButton>
+              </Form>
+            )}
+          </Formik>
+        </FormContainer>
+
         <Button>Alunos</Button>
-        <Button>Arquivos</Button>
+        <Button onClick={showFiles}>Arquivos</Button>
+
       </ContentPrimary>
+
+      <FilePanel isVisible={isPanelVisible}>
+        <PanelHeaderContainer>
+          <h2>Arquivos da Aula</h2>
+          <IoClose onClick={hideFiles} size={30} style={{cursor: 'pointer'}}/>
+        </PanelHeaderContainer>
+        
+        <FilesList>
+          {MockFiles.map((file) => (
+            <File>
+              <Link href={file.url}>{file.name} - {file.type}</Link>
+              
+              <ButtonsContainer>
+                <FaRegEdit onClick={hideFiles} size={24} style={{cursor: 'pointer'}} />
+                <FaRegTrashAlt onClick={hideFiles} size={24} style={{cursor: 'pointer'}} />
+              </ButtonsContainer>
+            </File>
+          ))}
+        </FilesList>
+      </FilePanel>
+
       <ContentSecondary>
-        <Button primary>Gerar Código</Button>
-        <GeneratedCode>
+        <Button onClick={() => generateCode()} primary>Gerar Código</Button>
+        <GeneratedCode code={code}>
           Código da aula gerado com sucesso
           <br />
-          <strong>024593</strong>
+          <strong>{code}</strong>
         </GeneratedCode>
       </ContentSecondary>
     </Container>
--- src/pages/StartClass/styles.ts
+++ src/pages/StartClass/styles.ts
@@ -13,6 +13,11 @@ export const Container = styled.div`
   padding: 20px 0;
 `;
 
+export const Title = styled.h2`
+  margin: 30px 150px;
+  font-weight: normal;
+`;
+
 export const MenuItem = styled.div`
   font-size: 16px;
   margin-bottom: 10px;
@@ -33,7 +38,7 @@ export const Button = styled.button<{ primary?: boolean }>`
   margin: 10px 5px;
   cursor: pointer;
   font-size: 16px;
-  width:200px;
+  width:150px;
   &:hover {
     opacity: 0.9;
   }
@@ -45,7 +50,49 @@ export const ContentPrimary = styled.div`
   margin 0 auto;
   justify-content: space-between;
   align-items: center;
-  width: 700px;
+`;
+
+export const FilePanel = styled.div<{ isVisible: boolean }>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 500px;
+  background-color: ${colors.secondBackground};
+  color: white;
+  transform: translateX(${props => (props.isVisible ? '0' : '100%')});
+  transition: transform 0.3s ease-in-out;
+  z-index: 1000;
+  padding: 20px;
+  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.2);
+`;
+
+export const PanelHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+export const FilesList = styled.ul`
+  list-style: none;
+`;
+
+export const File = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const Link = styled.a`
+  color: ${colors.text};
+  text-decoration: none;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 10px;
 `;
 
 export const ContentSecondary = styled.div`
@@ -57,8 +104,8 @@ export const ContentSecondary = styled.div`
   align-items: center;
 `;
 
-
-export const GeneratedCode = styled.div`
+export const GeneratedCode = styled.div<{ code?: string }>`
+  display: ${(props) => (props.code ? 'block' : 'none')};
   text-align: center;
   margin-top: 20px;
   font-size: 18px;
@@ -68,5 +115,46 @@ export const GeneratedCode = styled.div`
     display: block;
     margin-top: 10px;
     color: #fff;
+    letter-spacing: 5px;
+  }
+`;
+
+export const FormContainer = styled.div`
+  margin-right: 50px;
+`;
+
+export const InputContainer = styled.div`
+  margin-right: -10px;
+`;
+
+export const SelectInput = styled.select`
+  background-color: ${colors.secondBackground};
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+  color: ${colors.text};
+  font-size: 12pt;
+  padding: 10px 10px;
+  width: 350px;
+`;
+
+export const Error = styled.div`
+  color: ${colors.deleteButtonBackground};
+  margin-bottom: -25px;
+`;
+
+export const SubmitButton = styled.button`
+  display: block;
+  border: none;
+  border-radius: 5px;
+  background-color: ${colors.buttonBackground};
+  color: ${colors.buttonText};
+  font-size: 16px;
+  width: 150px;
+  padding: 10px 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${colors.buttonBackgroundHover}
   }
 `;
